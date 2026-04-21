3531a611985e3b756c18139d6c79786594908703
Hilanai <hilagoncruz@gmail.com>
2026-04-20 22:48:22 -0600

    commit mejora en sp y controllers

--- Models/UtilitarioModel.php
+++ Models/UtilitarioModel.php
@@ -3,7 +3,7 @@
 function OpenDatabase()
 {
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    return mysqli_connect("localhost", "root", "root", "CasoEstudioMN");
+    return mysqli_connect("localhost", "root", "", "CasoEstudioMN");
 }
 
 function CloseDatabase($context)
